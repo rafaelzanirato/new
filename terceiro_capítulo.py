fba364c205285d8e43d20770d25de8a22ecf5cf1
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-05-26 17:00:31 -0300

    Add files via upload

---
+++ terceiro_capítulo.py
@@ -0,0 +1,24 @@
+teams=["barcelona", "psg", "city", "chelsea", "psv"]
+print(teams)
+print(teams[3].title())
+teams[4]= "feyenoord"
+print(teams)
+teams.append("liverpool")
+print(teams)
+teams.insert(2,"lokomotiv moscow")
+print (teams)
+del teams[5]
+print(teams)
+you=teams.pop()
+print(teams)
+print(you)
+we=teams.pop(0)
+print("I like "+we)
+third=teams.pop(2)
+print(teams)
+out=["fire","water","air","ground"]
+print(out)
+out.remove("ground")
+print(out)
+out.sort()
+print(out)
